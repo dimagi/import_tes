46a5726c3d8eec76a7b6ff603ac56d461a88677a
Norman Hooper <nhooper@dimagi.com>
2020-07-10 17:31:13 +0200

    --wip-- Settings for sites. Use env vars.

--- import_teis.py
+++ import_teis.py
@@ -1,76 +1,91 @@
 #!/usr/bin/env python3
 """
-Example script to demonstrate bulk-importing DHIS2 tracked entities as
+Imports "Quarantine Site" tracked entity instances as "facility"
 CommCare cases.
 
-Usage:
-1. Customize constants at the start of the script.
-2. Customize the ``get_name()`` function if necessary.
-3. Run::
+IDs are specific to the staging.dhis.hisp.org server.
 
-        $ python3 import_teis.py
+The script requires the following environment variables to be set:
+
+* DHIS2_USERNAME
+* DHIS2_PASSWORD
+* COMMCARE_USERNAME
+* COMMCARE_PASSWORD
 
 """
-# TODO: Configure the following constants:
+import os
+import sys
+from contextlib import contextmanager
+from tempfile import TemporaryFile
+from typing import Iterable
 
-# CommCare authentication details:
-COMMCARE_PROJECT_SPACE = 'my-project-space'
-COMMCARE_USERNAME = 'user@example.com'
-COMMCARE_PASSWORD = 's3cret'
+import requests
+import tablib
 
-# The CommCare case type to be imported:
-COMMCARE_CASE_TYPE = 'person'
+DHIS2_BASE_URL = 'https://staging.dhis.hisp.org/sl-idsr/'
+DHIS2_PAGE_SIZE = 50
+COMMCARE_BASE_URL = 'https://www.commcarehq.org/'
+COMMCARE_PROJECT_SPACE = 'sl-demo'
+COMMCARE_CASE_TYPE = 'facility'
 
 # Map DHIS2 tracked entity attributes and JSON properties to CommCare
 # case properties.
 CASE_PROPERTY_MAP = {
     # DHIS2 TEI property (as given in JSON): CommCare case property
+
+    # NOTE: external_id is set to DHIS2 UID, not Site ID
     'trackedEntityInstance': 'external_id',
+
+    # NOTE: DHIS2 orgUnit maps to CommCare owner; Cases are owned by
+    #       their district.
     'orgUnit': 'owner_id',
+
     'attributes': {
-        'zDhUuAYrxNC': 'last_name',
-        'w75KJ2mc4zz': 'first_name',
+        'u440jMnMwBd': 'dhis2_name',  # Name
+        'IykD2cA7f1Z': 'dhis2_type',  # Type
+        'eqf88skHviu': 'dhis2_manager',  # Manager
+        'OT9sulhuFMI': 'dhis2_mobile_no_1',  # Mobile No 1
+        'zefVkrKFzbi': 'dhis2_num_in_quarantine',  # Number in quarantine
+        'M4Si4G6a31S': 'dhis2_start_date',  # Start date
+        'AQpLLbcDBno': 'dhis2_district',  # District
+        'QijMa435eiK': 'dhis2_address',  # Address
+        'kS05m3nd7cy': 'dhis2_zone',  # Chiefdom/Zone/City
+        'zIKV6Bdr8k6': 'dhis2_adults_20plus',  # Adults 20+ years
+        'hOT8lGKKFQG': 'dhis2_children_5to19',  # Children 5-19 years
+        'omixBEAEJxA': 'dhis2_children_under5',  # Children under 5 years
+        'jOODHzJzWde': 'dhis2_remarks',  # Remarks
     }
 }
 
 # Map DHIS2 organisation units to CommCare locations.
 ORG_UNIT_MAP = {
     # DHIS2 OrgUnit ID: CommCare location ID
-    'O6uvpzGd5pu': 'c0ffeeee-b450-4f9c-9fdb-c8f855f0c531',  # Bo
-    # Bombali
-    # Bonthe
-    # Kailahun
-    # Kambia
-    # Kenema
-    # Koinadugu
-    # Kono
-    # Moyamba
-    # Port Loko
-    # Pujehun
-    # Tonkolili
-    # Western Area
+    # 'Plmg8ikyfrK': 'not-a-real-location-id',  # Sierra Leone
+    'zTHjIvPmPI0': 'b8c3c4514b9d4f2e8057ef51e14c3736',  # Bo District
+    'ucyuSTA19cP': '0d028b468f584d7e98908036fecc5bf8',  # Bombali District
+    'iQgaTATK59f': '635b027b51f1497b85b79623f6903372',  # Bonthe District
+    'pWb43ue4nt8': '031d11add3bd4426a405b2c6f82f946f',  # Falaba District
+    'oWaJJJLCWzI': '79af11227ba74f25acca227fdffc543c',  # Kailahun District
+    'Ba6McOxy6D2': '1f7a65aec4dc4f209bca50356620fda9',  # Kambia District
+    'x4tRoN2ue9w': 'b692666cf2994a2aad85363b30e36568',  # Karene District
+    'gheAIXur8EJ': '175024c160474ac6aaa84a070a850d8d',  # Kenema District
+    'Y7YrGOsu9fp': 'b069228c635a4885aa3c105cc6eabf6c',  # Koinadugu District
+    'P7LlNb9MosU': 'e16a8b8b93f7488b9c2eceb5f6ffad8d',  # Kono District
+    'btW1NRETLww': '74a29d76d0dc490f80269b09dbc8416a',  # Moyamba District
+    'RjOUVJDV1Dl': '9709ddb2cfa9412ba4f0bf1ec73cd392',  # Port Loko District
+    'ywV0ByvR7xW': 'badc81c939f54523a22668f79408b1e1',  # Pujehun District
+    'yA18sXAwZJo': '3aa2d8127850415fa66b06e9b2b9afc2',  # Tonkolili District
+    'knrO5gazmom': '96f0546c6c9243279ea4e7366c753d59',  # Western Area Rural District
+    'PMLlCzM0mWT': 'e8654ba06c0f4339a92c8bb99f4c2481',  # Western Area Urban District
 }
 
 # If a TEI has an organisation unit that is not mapped to a CommCare
-# location, should an error be raised? If set to `False`, the TEI will
+# owner, should an error be raised? If set to `False`, the TEI will
 # be skipped.
 RAISE_ERROR_ON_MISSING_ORG_UNIT = True
 
-# The URL of the DHIS2 server and path to the API (excluding "api/"):
-DHIS2_BASE_URL = 'https://play.dhis2.org/dev/'
-DHIS2_USERNAME = 'admin'
-DHIS2_PASSWORD = 'district'
 
-# API request parameters used for filtering the TEIs to be imported:
-TEI_REQUEST_PARAMS = {
-    'ou': 'ImspTQPwCqd',  # Sierra Leone
-    'trackedEntityType': 'nEenWmSyUEp',  # Person
-    # 'program': 'uy2gU8kT1jF',  # MNCH / PNC (Adult Woman)
-}
-
-
-# TODO: Confirm `get_name()` uses the right tracked entity attributes
-#       and returns the CommCare case name in the right format:
+# TODO: Fix this for Quarantine Sites
 def get_name(tracked_entity) -> str:
     """
     Returns a case name for a given ``tracked_entity``
@@ -99,69 +114,40 @@ def get_name(tracked_entity) -> str:
     elif given_names:
         return given_names[0]
 
-# End of configuration
-
-
-from contextlib import contextmanager
-from tempfile import TemporaryFile
-from typing import Iterable
-
-import requests
-import tablib
-
-COMMCARE_BASE_URL = 'https://www.commcarehq.org/'
-DHIS2_PAGE_SIZE = 50
-
 
 def get_tracked_entities_from_dhis2() -> Iterable[dict]:
     """
     Returns an iterable of dictionaries, as returned by the DHIS2 web
     API. (Can be a list or a generator. A generator could be useful if
     the function handles pagination.)
-
-    A tracked entity looks a bit like this. (Some properties have been
-    omitted.) ::
-
-        {
-            'orgUnit': 'ImspTQPwCqd',
-            'trackedEntityInstance': 'iHhCKKXHQv6',
-            'trackedEntityType': 'nEenWmSyUEp',
-            'attributes': [
-                {
-                    'displayName': 'Last name',
-                    'attribute': 'zDhUuAYrxNC',
-                    'value': 'May'
-                },
-                {
-                    'displayName': 'First name',
-                    'attribute': 'w75KJ2mc4zz',
-                    'value': 'Isabel'
-                }
-            ]
-        }
-
     """
     endpoint = '/api/trackedEntityInstances'
     url = prefix_base_url(DHIS2_BASE_URL, endpoint)
     params = {
-        **TEI_REQUEST_PARAMS,
+        'trackedEntityType': 'qtKp6wnGc1L',  # Site
+        # 'program': 'MjFF9x9Cka9',  # Quarantine Site Daily Monitoring
         'paging': 'True',
         'pageSize': DHIS2_PAGE_SIZE,
-        'page': 1,
     }
     headers = {'Accept': 'application/json'}
-    auth = (DHIS2_USERNAME, DHIS2_PASSWORD)
-    while True:
-        response = requests.get(url, params, headers=headers, auth=auth)
-        teis = response.json()['trackedEntityInstances']
-        for tei in teis:
-            yield tei
-        if len(teis) < DHIS2_PAGE_SIZE:
-            # The "trackedEntityInstances" endpoint does not give us
-            # paging data like some other endpoints. So if we didn't get
-            # a full page of results, we know we're on the last page.
-            return
-        params['page'] += 1
+    auth = (os.environ['DHIS2_USERNAME'], os.environ['DHIS2_PASSWORD'])
+    for ou in ORG_UNIT_MAP:
+        params.update({
+            'ou': ou,
+            'page': 1,
+        })
+        while True:
+            response = requests.get(url, params, headers=headers, auth=auth)
+            teis = response.json()['trackedEntityInstances']
+            for tei in teis:
+                yield tei
+            if len(teis) < DHIS2_PAGE_SIZE:
+                # The "trackedEntityInstances" endpoint does not give us
+                # paging data like some other endpoints. We know we're
+                # on the last page if we didn't get a full page of
+                # results.
+                break
+            params['page'] += 1
 
 
 def map_tracked_entity_attributes(tracked_entities) -> Iterable[dict]:
@@ -169,26 +155,6 @@ def map_tracked_entity_attributes(tracked_entities) -> Iterable[dict]:
     Takes an iterable of tracked entities, and returns an interable of
     dictionaries with tracked entity attributes mapped to case property
     values.
-
-    e.g. ::
-
-        >>> generator = map_tracked_entity_attributes([{
-        ...     'trackedEntityInstance': 'iHhCKKXHQv6',
-        ...     'orgUnit': 'O6uvpzGd5pu',
-        ...     'attributes': [
-        ...         {'attribute': 'zDhUuAYrxNC', 'value': 'May'},
-        ...         {'attribute': 'w75KJ2mc4zz', 'value': 'Isabel'}
-        ...     ]
-        ... }])
-        >>> list(generator) == [{
-        ...     'name': 'MAY, Isabel',
-        ...     'external_id': 'iHhCKKXHQv6',
-        ...     'owner_id': 'c0ffeeee-b450-4f9c-9fdb-c8f855f0c531',
-        ...     'last_name': 'May',
-        ...     'first_name': 'Isabel',
-        ... }]
-        True
-
     """
     for tracked_entity in tracked_entities:
         dhis2_id_property = CASE_PROPERTY_MAP['trackedEntityInstance']
@@ -250,7 +216,8 @@ def bulk_upload_cases(tempfile):
     Uploads case data stored in ``tempfile`` to CommCare HQ. Returns a
     status URL if upload succeeds. Raises an exception if upload fails.
     """
-    endpoint = f'/a/{COMMCARE_PROJECT_SPACE}/importer/excel/bulk_upload_api/'
+    commcare_project_space = os.environ['COMMCARE_PROJECT_SPACE']
+    endpoint = f'/a/{commcare_project_space}/importer/excel/bulk_upload_api/'
     url = prefix_base_url(COMMCARE_BASE_URL, endpoint)
     data = {
         'case_type': COMMCARE_CASE_TYPE,
@@ -260,7 +227,7 @@ def bulk_upload_cases(tempfile):
         'comment': 'Imported from DHIS2 tracked entities',
     }
     files = {'file': (f'{COMMCARE_CASE_TYPE}_cases.xlsx', tempfile)}
-    auth = (COMMCARE_USERNAME, COMMCARE_PASSWORD)
+    auth = (os.environ['COMMCARE_USERNAME'], os.environ['COMMCARE_PASSWORD'])
     response = requests.post(url, data, files=files, auth=auth)
     response.raise_for_status()
     return response.json()['status_url']
@@ -282,7 +249,23 @@ def prefix_base_url(base_url, endpoint):
     return '/'.join((base_url.rstrip('/'), endpoint.lstrip('/')))
 
 
+def get_missing_env_vars():
+    env_vars = (
+        'DHIS2_USERNAME',
+        'DHIS2_PASSWORD',
+        'COMMCARE_USERNAME',
+        'COMMCARE_PASSWORD',
+    )
+    return [v for v in env_vars if v not in os.environ]
+
+
 if __name__ == '__main__':
+    missing = get_missing_env_vars()
+    if missing:
+        print('These required environment variables are not set:',
+              ', '.join(missing))
+        sys.exit(1)
+
     tracked_entities = get_tracked_entities_from_dhis2()
     cases = map_tracked_entity_attributes(tracked_entities)
     with save_cases(cases) as tempfile:
