d8f9daebe78832f77a0f9c7a49c4367fdee9e467
Norman Hooper <nhooper@dimagi.com>
2020-10-23 18:46:44 +0200

    Rename script

--- tests.py
+++ tests.py
@@ -1,13 +1,13 @@
 import doctest
 import unittest
 
-import import_teis
+import import_facilities
 
 
 class DocTests(unittest.TestCase):
 
     def test_doctests(self):
-        results = doctest.testmod(import_teis)
+        results = doctest.testmod(import_facilities)
         self.assertEqual(results.failed, 0)
 
 
